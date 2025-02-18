37ddf79e46eea23d4bea71e0c05cd92f7da27dce
Bojan Crnovcic <bojan969@gmail.com>
2025-02-18 17:00:56 +0100

    prblem supabse

--- src/misc/supabase.client.ts
+++ src/misc/supabase.client.ts
@@ -3,8 +3,8 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KAY = process.env.SUPABASE_ANON_KAY;
+const SUPABASE_URL = 'https://xzfbzeucuacmhxegnpir.supabase.co';
+const SUPABASE_ANON_KAY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6ZmJ6ZXVjdWFjbWh4ZWducGlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1NTU1MjEsImV4cCI6MjA1NTEzMTUyMX0.N1_ojpA7Ieza6WM9oglo7BMPMbySWnXVhQ0KEAl6-LY';
 
 console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
 console.log("SUPABASE_ANON_KAY:", process.env.SUPABASE_ANON_KAY);
